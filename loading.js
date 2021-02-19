3ea820ddfbed87e6b7f419c242c39afae608b4e1
theCYBERbro <theCYBERbro@users.noreply.github.com>
2021-02-19 16:37:53 +0200

    🥵 Added loading screen animation

---
+++ loading.js
@@ -0,0 +1,10 @@
+//autoloading on load screen
+
+window.onload = function() {
+        $(".se-pre-con").fadeOut("slow");
+
+    }
+    // $(window).load(function() {
+    //     // Animate loader off screen
+    //     $(".se-pre-con").fadeOut("slow");
+    // });
